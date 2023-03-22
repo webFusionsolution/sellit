73545de0144f6925c1ce70d2cb2fa1151127be59
DESKTOP-72D0N0M\Dell <webfusionsolution@gmail.com>
2023-03-22 15:42:22 +0530

    Updated order api file

--- routes/order.js
+++ routes/order.js
@@ -67,12 +67,17 @@ router.get('/', verifyTokenandAdmin, async (req, res) => {
 // get monthly income
 
 router.get('/income', verifyTokenandAdmin, async (req, res) => {
+    const productId = req.query.pid;
     const date = new Date();
     const lastMonth = new Date(date.setMonth(date.getMonth() - 1))
     const previousMonth = new Date(new Date().setMonth(lastMonth.getMonth() - 1))
     try {
         const income = await Order.aggregate([
-            { $match: { createdAt: { $gte: previousMonth } } },
+            { $match: { createdAt: { $gte: previousMonth }, ...(productId && {
+                products: {
+                    $elemMatch: {productId: productId}
+                }
+            }) } },
             {
                 $project: {
                     month: { $month: "$createdAt" },
